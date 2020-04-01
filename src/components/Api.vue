187041418b9c3f84e750a763f6b005918f010c47
PaulMaldonado <paul_pastore10@hotmail.com>
2020-03-31 22:36:45 -0600

    My first commit

---
+++ src/components/Api.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+         <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Ricky & Morthy</h1>
+                <p class="lead">Ricky and Morthy personajes</p>
+
+                <button class="btn btn-primary" @click="printResults">Consultar datos</button>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col-md-4 col-sm-12 col-lg-4 col-xl-4" v-for="character in characters" :key="character.id">
+                    <div class="card mt-3">
+                        <img v-bind:src="character.image" v-bind:alt="character" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ character.name }}</h5>
+                            <p class="card-text">{{ character.gender }}</p>
+                            <h6 class="card-text">{{ character.status}}</h6>
+
+                            <button class="btn btn-danger ustify-content-around">Eliminar</button>
+                            <button class="btn btn-success">Ver más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            url: 'https://rickandmortyapi.com/api/character',
+            characters: [],
+        }
+    },
+
+    methods: {
+        printResults() {
+            axios.get(this.url)
+                .then(response => {
+                    this.characters = response.data.results
+                    console.log(response.data.results)
+                })
+
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+         <a class="navbar-brand" href="#">{{ titleNav }}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            titleNav: 'Ricky & Morthy Api',
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
